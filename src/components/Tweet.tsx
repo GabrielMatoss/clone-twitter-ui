7370b64a2cceccfbee49fd242292b98c5eb39965
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-02-07 20:57:06 -0300

    iniciando html dos tweets

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,5 +1,16 @@
+import "./Tweet.css";
+
 export function Tweet() {
     return(
-        <div>Tweet</div>
+     <a href="#" className="tweet">
+        <img src="https://github.com/GabrielMatoss.png" alt="Gabriel Matos"/>
+
+        <div className="tweet-content">
+            <div className="tweet-content-header">
+                <strong>Gabriel Matos</strong>
+                <span>@gabrielmatoss</span>
+            </div>
+        </div>
+     </a>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import {
 } from "phosphor-react";
 
 import twitterLogo from "./assets/logo-twitter.svg";
+import { Tweet } from "./components/Tweet";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
@@ -75,6 +76,13 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 
             <button type="submit">Tweet</button>
           </form>
+
+          <div className="separator" />
+
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
         </main>
       </div>
     </div>
