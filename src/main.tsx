de5f4ad1865042719cf47658a3e9dad7dfdab5c5
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-02-07 18:04:58 -0300

    commit inicial com a estrutura do projeto

---
+++ src/main.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./global.css";
+import {
+  House,
+  Hash,
+  Bell,
+  Envelope,
+  BookmarksSimple,
+  FileText,
+  User,
+  DotsThreeCircle,
+  Sparkle,
+} from "phosphor-react";
+
+import twitterLogo from "./assets/logo-twitter.svg";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <div className="layout">
+      <aside className="sidebar">
+        <img className="logo" src={twitterLogo} alt="Logo" />
+
+        <nav className="main-navigation">
+          <a className="active" href="">
+            <House weight="fill" />
+            Home
+          </a>
+          <a href="">
+            <Hash />
+            Explore
+          </a>
+          <a href="">
+            <Bell />
+            Notifications
+          </a>
+          <a href="">
+            <Envelope />
+            Messages
+          </a>
+          <a href="">
+            <BookmarksSimple />
+            Bookmarks
+          </a>
+          <a href="">
+            <FileText />
+            List
+          </a>
+          <a href="">
+            <User />
+            Profile
+          </a>
+          <a href="">
+            <DotsThreeCircle />
+            More
+          </a>
+        </nav>
+
+        <button className="new-tweet" type="button">
+          Tweet
+        </button>
+      </aside>
+      <div className="content">
+        <main className="timeline">
+          <div className="timeline-header">
+            Home
+            <Sparkle />
+          </div>
+
+          <form className="new-tweet-form">
+            <label htmlFor="tweet">
+              <img src="https://github.com/GabrielMatoss.png" alt="Gabriel Matos" />
+              <textarea id="tweet" placeholder="What's happening?"/>
+            </label>
+
+            <button type="submit">Tweet</button>
+          </form>
+        </main>
+      </div>
+    </div>
+  </React.StrictMode>
+);
